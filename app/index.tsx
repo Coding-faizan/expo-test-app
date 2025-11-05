1d1b60e29bb77037b68ecab539864c37d93a2f39
Coding-faizan <faizanahmad8292@gmail.com>
2025-11-05 16:59:24 +0500

    fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,21 @@
+import { StatusBar } from "expo-status-bar";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </SafeAreaView>
+    <>
+      <StatusBar style="auto" />
+      <SafeAreaView
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text style={{ fontFamily: "Inter_300Light_Italic" }}>Inter Black</Text>
+      </SafeAreaView>
+    </>
   );
 }
