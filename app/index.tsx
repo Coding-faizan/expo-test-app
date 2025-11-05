a1eeb16946b3dcf61d661d9b9356f42ef46605a6
Coding-faizan <faizanahmad8292@gmail.com>
2025-11-05 18:46:03 +0500

    color theme

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,46 @@
-import { Image } from "expo-image";
+import { StyleSheet, Text, View, useColorScheme } from "react-native";
+
 import { StatusBar } from "expo-status-bar";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const colorScheme = useColorScheme();
+
+  const themeTextStyle =
+    colorScheme === "light" ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === "light" ? styles.lightContainer : styles.darkContainer;
+
   return (
     <>
       <StatusBar style="auto" />
-      <SafeAreaView
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Image
-          source={require("../assets/images/react-logo.png")}
-          style={{ height: 100, width: 100 }}
-        />
-        <Text>Edit app/index.tsx to edit this screen.</Text>
-        <Text style={{ fontFamily: "Inter_300Light_Italic" }}>Inter Black</Text>
-      </SafeAreaView>
+      <View style={[styles.container, themeContainerStyle]}>
+        <Text style={[styles.text, themeTextStyle]}>
+          Color scheme: {colorScheme}
+        </Text>
+      </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+  },
+  lightContainer: {
+    backgroundColor: "#d0d0c0",
+  },
+  darkContainer: {
+    backgroundColor: "#242c40",
+  },
+  lightThemeText: {
+    color: "#242c40",
+  },
+  darkThemeText: {
+    color: "#d0d0c0",
+  },
+});
